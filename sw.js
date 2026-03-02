632dd063cddf51816a524f7b9252f197a41abc41
woutervdboogaard-cmyk <wouter.vd.boogaard@gmail.com>
2026-03-02 20:36:01 +0100

    Fix navigation and audio initialization on main page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="vlotvast-v9-1";
+const CACHE="vlotvast-v9-2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -17,4 +17,4 @@ const ASSETS = [
 ];
 self.addEventListener("install",(e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate",(e)=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))); self.clients.claim(); });
-self.addEventListener("fetch",(e)=>{ e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request).catch(()=>caches.match("./index.html")))); });
+self.addEventListener("fetch",(e)=>{ e.respondWith(caches.match(e.request, { ignoreSearch:true }).then(r=>r || fetch(e.request).catch(()=>caches.match("./index.html")))); });
