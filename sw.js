8e9c49ab80b18ac71e896c5a06378875a87dfa78
woutervdboogaard-cmyk <wouter.vd.boogaard@gmail.com>
2026-03-02 20:45:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
 diff --git a/sw.js b/sw.js
-index 749274bdfe6b6da9eede35f47784f63894ce2696..accfcea51d85f9021af540e4c28265e8c0e7fed2 100644
+index 749274bdfe6b6da9eede35f47784f63894ce2696..a300c2d70c1c197b2b60004ee05aea8506cfa744 100644
 --- a/sw.js
 +++ b/sw.js
 @@ -1,20 +1,20 @@
@@ -24,8 +23,4 @@ index 749274bdfe6b6da9eede35f47784f63894ce2696..accfcea51d85f9021af540e4c28265e8
  ];
  self.addEventListener("install",(e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
  self.addEventListener("activate",(e)=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))); self.clients.claim(); });
--self.addEventListener("fetch",(e)=>{ e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request).catch(()=>caches.match("./index.html")))); });
-+self.addEventListener("fetch",(e)=>{ e.respondWith(caches.match(e.request, { ignoreSearch:true }).then(r=>r || fetch(e.request).catch(()=>caches.match("./index.html")))); });
- 
-EOF
-)
+ self.addEventListener("fetch",(e)=>{ e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request).catch(()=>caches.match("./index.html")))); });
